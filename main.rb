e90c7b402accdb73485679edf9992100072147be
Patrick Jones <pmichaeljones@gmail.com>
2014-04-29 12:55:42 -0600

    Added hit & stay routes

--- main.rb
+++ main.rb
@@ -7,7 +7,7 @@ get '/' do
   erb :welcome
 end
 
-post '/set_name' do
+post '/start_game' do
   session[:username] = params[:username]
   suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
   cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
@@ -15,16 +15,26 @@ post '/set_name' do
   3.times { session[:deck].shuffle! }
 
   session[:dealer_cards] = []
-  session[:player_cards] = session[:deck].pop
-  session[:dealer_cards] = session[:deck].pop
-  session[:player_cards] = session[:deck].pop
-  session[:dealer_cards] = session[:deck].pop
+  session[:player_cards] = []
+  session[:player_cards] << session[:deck].pop
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
+  session[:dealer_cards] << session[:deck].pop
 
   erb :betting_form
 end
 
 get '/player_profile' do
   erb :"/PlayerViews/player_profile"
+end
+
+get '/player_hit' do
+  session[:player_cards] << session[:deck].pop
+  erb :betting_form
+end
+
+get '/player_stay' do
+  erb :betting_form
 end
 
 
