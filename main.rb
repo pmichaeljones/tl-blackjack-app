ffe865f0127012da362bc8a532589def1325807b
Patrick Jones <pmichaeljones@gmail.com>
2014-04-29 11:04:15 -0600

    Added routes

--- main.rb
+++ main.rb
@@ -3,6 +3,19 @@ require 'sinatra'
 
 set :sessions, true
 
+get '/' do
+  erb :welcome
+end
+
+post '/set_name' do
+  session[:username] = params[:username]
+  erb :betting_form
+end
+
+get '/player_profile' do
+  erb :"/PlayerViews/player_profile"
+end
+
 
 
 
