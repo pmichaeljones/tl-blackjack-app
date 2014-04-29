c1f36b7bb04e5457092d16e0dd4079142f8ea51f
Patrick Jones <pmichaeljones@gmail.com>
2014-04-29 12:24:36 -0600

    Added cards

--- main.rb
+++ main.rb
@@ -9,6 +9,13 @@ end
 
 post '/set_name' do
   session[:username] = params[:username]
+  suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
+  cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+  session[:deck] = suits.product(cards)
+  session[:deck].shuffle!
+
+  session[:player_cards] = session[:deck].pop(2)
+
   erb :betting_form
 end
 
