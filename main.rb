2d04a00d8365f6a3e67d12e70ea3861bf8200e4c
Patrick Jones <pmichaeljones@gmail.com>
2014-05-15 13:35:35 -0600

    finished betting

--- main.rb
+++ main.rb
@@ -85,6 +85,9 @@ post '/game' do
   if params[:bet].to_i > session[:pot]
     @outcome_lose = "You Can't Bet More Than You Have."
     halt erb :input_bet
+  elsif params[:bet].to_i == 0
+    @outcome_lose = "You need to bet a number greater than 0."
+    halt erb :input_bet
   else
     session[:bet] = params[:bet].to_i
     session[:pot] = session[:pot] - session[:bet]
@@ -103,6 +106,14 @@ post '/game' do
 
   session[:player_score] = calculate_total(session[:player_cards])
 
+  if session[:player_score] == BLACKJACK_AMOUNT
+    session[:pot] = session[:pot] + session[:bet] * 3
+    @outcome_win = "You got BlackJack and won #{session[:bet] * 3}!"
+    @action_buttons = false
+    @play_again = true
+  end
+
+
   erb :game
 end
 
@@ -111,6 +122,11 @@ post '/player_hit' do
   session[:player_score] = calculate_total(session[:player_cards])
   if session[:player_score] > BLACKJACK_AMOUNT
     @outcome_lose = "You Busted with a #{session[:player_score]}!"
+    @action_buttons = false
+    @play_again = true
+  elsif session[:player_score] == BLACKJACK_AMOUNT
+    session[:pot] = session[:pot] + session[:bet] * 3
+    @outcome_win = "You got BlackJack and won #{session[:bet] * 3}!"
     @action_buttons = false
     @play_again = true
   end
@@ -124,17 +140,20 @@ post '/player_stay' do
   @action_buttons = false
   session[:dealer_score] = calculate_total(session[:dealer_cards])
 
-  if session[:dealer_score] >= DEALER_MUST_HIT && session[:dealer_score] > session[:player_score]
-    @outcome_lose = "Dealer's #{session[:dealer_score]} beats player's #{session[:player_score]}. Dealer Wins."
+  if session[:dealer_score] > DEALER_MUST_HIT && session[:dealer_score] > session[:player_score]
+    @outcome_lose = "Dealer's #{session[:dealer_score]} beats #{session[:player]}'s #{session[:player_score]}. Dealer Wins."
     @play_again = true
-    session[:pot] = session[:pot]
 
-  elsif session[:dealer_score] >= DEALER_MUST_HIT && session[:player_score] > session[:dealer_score]
+  elsif session[:dealer_score] > DEALER_MUST_HIT && session[:dealer_score] == session[:player_score]
+    @outcome_lose = "It's a Push at #{session[:dealer_score]}. House wins."
+    @play_again = true
+
+  elsif session[:dealer_score] > DEALER_MUST_HIT && session[:player_score] > session[:dealer_score]
     @outcome_win = "#{session[:player]}'s #{session[:player_score]} beat's the dealer's #{session[:dealer_score]}."
     @play_again = true
     session[:pot] = session[:pot] + session[:bet] * 2
 
-  elsif session[:dealer_score] < DEALER_MUST_HIT
+  elsif session[:dealer_score] <= DEALER_MUST_HIT
     @dealer_turn = true
   end
 
@@ -142,8 +161,47 @@ post '/player_stay' do
 
 end
 
+post '/dealer_hit' do
+  @dealer_show = true
+  @action_buttons = false
+  session[:dealer_cards] << session[:deck].pop
+  session[:dealer_score] = calculate_total(session[:dealer_cards])
+
+  if session[:dealer_score] > BLACKJACK_AMOUNT
+    @outcome_win = "Dealer busted! #{session[:player]} wins!"
+    session[:pot] = session[:pot] + session[:bet] *2
+    @play_again = true
+
+  elsif session[:dealer_score] > DEALER_MUST_HIT && session[:dealer_score] > session[:player_score]
+    @outcome_lose = "Dealer's #{session[:dealer_score]} beats player's #{session[:player_score]}. Dealer Wins."
+    @play_again = true
+
+  elsif session[:dealer_score] > DEALER_MUST_HIT && session[:dealer_score] == session[:player_score]
+    @outcome_lose = "It's a Push at #{session[:dealer_score]}. House wins."
+    @play_again = true
+
+  elsif session[:dealer_score] > DEALER_MUST_HIT && session[:player_score] > session[:dealer_score]
+    @outcome_win = "#{session[:player]}'s #{session[:player_score]} beat's the dealer's #{session[:dealer_score]}."
+    @play_again = true
+    session[:pot] = session[:pot] + session[:bet] * 2
+
+  elsif session[:dealer_score] <= DEALER_MUST_HIT
+    @dealer_turn = true
+  end
+
+  erb :game
+
+end
+
+
 post '/play_again' do
+  if session[:pot] == 0
+    @good_terms = false
+    halt erb :goodbye
+  else
   erb :input_bet
+  end
+
 end
 
 post '/end_game' do
